a6b82ba735635aae32620b298af3c8a2b16d9dae
OyubJun <c567tiuwertcx@outlook.com>
2024-08-09 15:05:23 +0400

    Upload Files

---
+++ Xyn-SeedWatcher/BalanceCheckerWin/HttpWebResponseExt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+
+namespace BalanceCheckerWin
+{
+	// Token: 0x0200000F RID: 15
+	public static class HttpWebResponseExt
+	{
+		// Token: 0x060008B7 RID: 2231 RVA: 0x0001DD88 File Offset: 0x0001BF88
+		public static HttpWebResponse GetResponseNoException(this HttpWebRequest req)
+		{
+			HttpWebResponse result;
+			try
+			{
+				result = (HttpWebResponse)req.GetResponse();
+			}
+			catch (WebException ex)
+			{
+				HttpWebResponse httpWebResponse = ex.Response as HttpWebResponse;
+				if (httpWebResponse == null)
+				{
+					throw;
+				}
+				result = httpWebResponse;
+			}
+			return result;
+		}
+	}
+}
