8f4985225c9311eb893f072d1bcccae9e48fadb8
OyubJun <c567tiuwertcx@outlook.com>
2024-08-09 15:05:44 +0400

    Upload Files

---
+++ Xyn-SeedWatcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("4232a12a-6a8f-4905-8725-f839c2d16606")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyProduct("SeedWatcher")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SeedWatcher")]
+[assembly: AssemblyCompany("")]
+[module: SuppressIldasm]
