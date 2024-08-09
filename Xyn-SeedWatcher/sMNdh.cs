84c6d12ec273659941c87e542cc66aa0c5e10dcd
OyubJun <c567tiuwertcx@outlook.com>
2024-08-09 15:05:11 +0400

    Upload Files

---
+++ Xyn-SeedWatcher/sMNdh.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// Token: 0x02000011 RID: 17
+internal class sMNdh : Attribute, <Module>, sMNdh
+{
+	// Token: 0x060008B9 RID: 2233 RVA: 0x000027DC File Offset: 0x000009DC
+	public sMNdh(string A_1) : this()
+	{
+	}
+
+	// Token: 0x060008BA RID: 2234 RVA: 0x000027E4 File Offset: 0x000009E4
+	public sMNdh(string A_1)
+	{
+		this..cctor();
+	}
+}
