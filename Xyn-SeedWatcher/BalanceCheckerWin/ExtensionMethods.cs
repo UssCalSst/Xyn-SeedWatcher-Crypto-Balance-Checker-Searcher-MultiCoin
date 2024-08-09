3a1fe127e640fadd55a631bfccb1969d9bbe9994
OyubJun <c567tiuwertcx@outlook.com>
2024-08-09 15:05:15 +0400

    Upload Files

---
+++ Xyn-SeedWatcher/BalanceCheckerWin/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace BalanceCheckerWin
+{
+	// Token: 0x0200000E RID: 14
+	public static class ExtensionMethods
+	{
+		// Token: 0x060008B6 RID: 2230 RVA: 0x000027A5 File Offset: 0x000009A5
+		public static void DoubleBuffered(this DataGridView dgv, bool setting)
+		{
+			dgv.GetType().GetProperty(<Module>.ZExtO("ᡛᡰᡪ᡽ᡳ᡺ᡝᡪ᡹᡹᡺ᡭ᡺᡻", <Module>.斯儿波留艾()), (BindingFlags)<Module>.大克儿大波()).SetValue(dgv, setting, null);
+		}
+	}
+}
