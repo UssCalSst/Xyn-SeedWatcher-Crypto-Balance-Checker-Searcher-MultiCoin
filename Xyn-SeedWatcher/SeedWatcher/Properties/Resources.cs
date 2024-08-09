25d28c4c50a858c15729f9a04b51da5478b789b6
OyubJun <c567tiuwertcx@outlook.com>
2024-08-09 15:05:46 +0400

    Upload Files

---
+++ Xyn-SeedWatcher/SeedWatcher/Properties/Resources.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace SeedWatcher.Properties
+{
+	// Token: 0x02000002 RID: 2
+	[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+	[DebuggerNonUserCode]
+	[CompilerGenerated]
+	internal class Resources
+	{
+		// Token: 0x06000847 RID: 2119 RVA: 0x00002054 File Offset: 0x00000254
+		internal Resources()
+		{
+		}
+
+		// Token: 0x17000001 RID: 1
+		// (get) Token: 0x06000848 RID: 2120 RVA: 0x0000205F File Offset: 0x0000025F
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static ResourceManager ResourceManager
+		{
+			get
+			{
+				if (Resources.resourceMan == null)
+				{
+					Resources.resourceMan = new ResourceManager(<Module>.ZExtO("泥泓泓泒泡泗泂法泞泓泄沘泦泄泙泆泓泄泂泟泓泅沘泤泓泅泙泃泄法泓泅", <Module>.豆埃波克儿()), typeof(Resources).Assembly);
+				}
+				return Resources.resourceMan;
+			}
+		}
+
+		// Token: 0x17000002 RID: 2
+		// (get) Token: 0x06000849 RID: 2121 RVA: 0x00002098 File Offset: 0x00000298
+		// (set) Token: 0x0600084A RID: 2122 RVA: 0x0000209F File Offset: 0x0000029F
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static CultureInfo Culture
+		{
+			get
+			{
+				return Resources.resourceCulture;
+			}
+			set
+			{
+				Resources.resourceCulture = value;
+			}
+		}
+
+		// Token: 0x04000001 RID: 1
+		private static ResourceManager resourceMan;
+
+		// Token: 0x04000002 RID: 2
+		private static CultureInfo resourceCulture;
+	}
+}
